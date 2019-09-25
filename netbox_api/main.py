f9c740c18aa42689fccf68caef9aa00c0eab3935
etigui <guignard.etienne@gmail.com>
2019-09-25 13:28:39 +0200

    Add credential

--- netbox_api/main.py
+++ netbox_api/main.py
@@ -4,8 +4,8 @@ class NBAPI():
     def __init__(self):
         self.__nb = pynetbox.api(
             'http://localhost:8000',
-            private_key_file='',
-            token=''
+            private_key_file='key/pk.key',
+            token='8ff35cbc1d7e1c4ef6d42e222b913c9f57bf7ed9'
         )
 
 if __name__ == "__main__":
